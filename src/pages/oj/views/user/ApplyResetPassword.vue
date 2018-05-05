89028de06fd48252cc6cee5bf6be77be571f71e7
Zhangrp <zrpxx@qq.com>
2018-05-05 15:02:30 +0800

    汉化完成

--- src/pages/oj/views/user/ApplyResetPassword.vue
+++ src/pages/oj/views/user/ApplyResetPassword.vue
@@ -4,14 +4,14 @@
     <template v-if="!successApply">
       <Form :rules="ruleResetPassword" :model=formResetPassword ref="formResetPassword">
         <Form-item prop="email">
-          <Input v-model="formResetPassword.email" placeholder="Your Email Address" size="large">
+          <Input v-model="formResetPassword.email" placeholder="Email地址" size="large">
           <Icon type="ios-email-outline" slot="prepend"></Icon>
           </Input>
         </Form-item>
         <Form-item prop="captcha" style="margin-bottom:10px">
           <div class="oj-captcha">
             <div class="oj-captcha-code">
-              <Input v-model="formResetPassword.captcha" placeholder="Captcha" size="large">
+              <Input v-model="formResetPassword.captcha" placeholder="验证码" size="large">
               <Icon type="ios-lightbulb-outline" slot="prepend"></Icon>
               </Input>
             </div>
@@ -26,13 +26,13 @@
       <Button type="primary"
               @click="sendEmail"
               class="btn" long
-              :loading="btnLoading">Send Password Reset Email
+              :loading="btnLoading">发送密码重置邮件
       </Button>
     </template>
     <template v-else>
       <Alert type="success" show-icon>
         Success
-        <span slot="desc">Password reset mail has been sent to your email.</span>
+        <span slot="desc">密码重置邮件已发送至您的邮箱</span>
       </Alert>
     </template>
   </Panel>
